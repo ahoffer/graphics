4ac449941fc6af84cd321ec919b0e82207969f59
Aaron <github@hoffer.org>
2023-01-11 12:37:44 -0800

    More refactoring

--- src/main/java/org/example/DrawingPanel.java
+++ src/main/java/org/example/DrawingPanel.java
@@ -9,6 +9,7 @@ public class DrawingPanel extends JPanel {
     public int originY = 0;
     public float ySkew = 1.0f;
     Surface3D surface;
+    private Graphics grxContext;
     private int x;
     private int xs;
     private int y;
@@ -22,40 +23,41 @@ public class DrawingPanel extends JPanel {
         surface = new Surface3D(50, 50, 150, -300, 300);
     }
 
-    private void drawGrid(Graphics g) {
+    private void drawGrid() {
         Point2D point2;
         Point2D point1;
         point1 = new Point3D(x * xs, (int) (surface.getElevationAvg(x, y) * ySkew), y * ys).transform3D(iso3D);
         point2 = new Point3D(x * xs, (int) (surface.getElevationAvg(x, y + 1) * ySkew), (y + 1) * ys).transform3D(iso3D);
-        g.drawLine(point1.x + originX, point1.y + originY, point2.x + originX, point2.y + originY);
+        grxContext.drawLine(point1.x + originX, point1.y + originY, point2.x + originX, point2.y + originY);
         System.err.printf("y-line from %s -> %s%n", point1, point2);
     }
 
-    private void drawNoise(Graphics g) {
+    private void drawNoise() {
         Point2D point1;
         Point2D point2;
         point1 = new Point3D(x * xs, (int) (surface.getElevationAvg(x, y) * ySkew), y * ys).transform3D(iso3D);
         point2 = new Point3D((x + 1) * xs, (int) (surface.getElevationAvg(x + 1, y) * ySkew), y * ys).transform3D(iso3D);
-        g.drawLine(point1.x + originX, point1.y + originY, point2.x + originX, point2.y + originY);
+        grxContext.drawLine(point1.x + originX, point1.y + originY, point2.x + originX, point2.y + originY);
         System.err.printf("noise line from %s -> %s%n", point1, point2);
     }
 
     public void paintComponent(Graphics g) {
+        this.grxContext = g;
         g.setColor(Color.black);
-        g.fillRect(0, 0, 1000, 1000);
-        g.setColor(Color.blue);
+        grxContext.fillRect(0, 0, 1000, 1000);
+        grxContext.setColor(Color.blue);
         Point2D point1 = null;
         Point2D point2 = null;
         xs = surface.xSquareSize;
         ys = surface.ySquareSize;
-        g.setColor(Color.green);
+        grxContext.setColor(Color.green);
         for (y = 0; y < surface.yGridSize; y++)
             for (x = 0; x < surface.xGridSize; x++) {
                 if (x < surface.xGridSize - 1) {
-                    drawNoise(g);
+                    drawNoise();
                 }
 //                if (y < surface.yGridSize - 1) {
-//                    drawGrid(g);
+//                    drawGrid();
 //                }
             }
     }
